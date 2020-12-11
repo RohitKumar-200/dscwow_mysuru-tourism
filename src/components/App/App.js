575a27a97fd3fc34c7562cd0f27a64b5726d754d
Rohit Kumar <lit2019037@iiitl.ac.in>
2020-12-11 16:18:16 +0530

    added login functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "../Login/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <h1>Welcome</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
