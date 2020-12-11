50a49214bca8807799323dbc54e8aad1b2fee89c
Mrhb787 <lit2019008@iiitl.ac.in>
2020-12-12 00:26:42 +0530

    New Component Added , Fixes for previous issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import Header from '../Header/Header';
-import Carosel from '../Carosel/Carosel';
-import Jumbo from '../Jumbo/Jumbo';
-import Footer from '../Footer/Footer'
+import Header from "../Header/Header";
+import Carosel from "../Carosel/Carosel";
+import Jumbo from "../Jumbo/Jumbo";
+import Travel from "../Travel/Travel";
+import Footer from "../Footer/Footer";
 function App() {
   return (
-  <div className="App">
-      <Header/>
-      <Carosel/>
-      <Jumbo/>
-      <Footer/>
-  </div>
+    <div className="App">
+      <Header />
+      <Carosel />
+      <Jumbo />
+      <Travel />
+      <Footer />
+    </div>
   );
 }
 
